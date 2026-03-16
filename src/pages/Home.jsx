77a72f63ff5839d2ad4c41494cdeb7ce97151296
Nicolaj <nicolaj@lalatoto.dk>
2026-03-16 16:14:50 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,6 +37,7 @@ function getGreeting(lang, gender) {
 }
 
 export default function Home() {
+  const headerVisible = useScrollDirection();
   const { t, lang } = useLanguage();
   const [user, setUser] = useState(null);
   const [, setCurrentTime] = useState(new Date());
