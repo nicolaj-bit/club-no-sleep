4ce2569d2f9693c6def110080ad8d08db2ab6a40
Nicolaj <nicolaj@lalatoto.dk>
2026-04-06 07:24:02 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
-import AboutUs from './pages/AboutUs';
+import AboutUs from './pages/AboutUs.jsx';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
