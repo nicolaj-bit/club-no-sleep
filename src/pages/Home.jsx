ef82c8541318b9883401f91ef9ada41217c7e2e2
Nicolaj <nicolaj@lalatoto.dk>
2026-05-13 13:42:53 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -81,7 +81,7 @@ export default function Home() {
 
   const { data: posts = [] } = useQuery({
     queryKey: ['blogPosts'],
-    queryFn: () => base44.entities.BlogPost.filter({ published: true }, '-published_date', 20),
+    queryFn: () => base44.entities.BlogPost.list('-published_date', 50).then(posts => posts.filter(p => p.published)),
   });
 
   const profile = activeProfile;
