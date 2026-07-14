47b4406b7cf3a4ff01c51916dd5154bfc6947fce
nicolaj-bit <nicolaj@lalatoto.dk>
2026-07-14 13:42:18 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,15 @@ const config: CapacitorConfig = {
   appId: 'com.base699f47a86e7e0a874d1159ed.app',
   appName: 'Club No Sleep',
   webDir: 'dist',
+  server: {
+    url: 'https://lalatoto.base44.app',
+    allowNavigation: [
+      'lalatoto.base44.app',
+      'clubnosleep.com',
+      'app.base44.com',
+      'base44.app',
+    ],
+  },
   ios: {
     contentInset: 'always',
   },
